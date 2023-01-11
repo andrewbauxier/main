eb1d175d2fe6d009384030b49140a8058d1249af
andrewbauxier <andrew.b.auxier@gmail.com>
2023-01-11 10:17:07 +0900

    Change file names and finish basic OOP

---
+++ school/java/assignments/discussion1/discussion1/src/discussion1.java
@@ -0,0 +1,7 @@
+public class discussion1 {
+    public static void main(String[] args) { //inputs = (String n, String speech, int numlegs, int numeyes, double weight)
+        Doggo doggo1 = new Doggo("Mr. Cuddlesworth", "Woof", 10, 4, 22.5);
+
+        doggo1.doggostatus();
+    }
+}
--- school/java/assignments/discussion1/discussion1/src/player.java
+++
@@ -1,4 +0,0 @@
-public class player {
-    public static void main(String[] args) {  
-    }
-}
