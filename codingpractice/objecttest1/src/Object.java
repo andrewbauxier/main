7d51b66784d4f03aa260bfa6cd39e90bede35066
andrewbauxier <andrew.b.auxier@gmail.com>
2023-01-13 11:58:11 +0900

    Move folders to another space and start new repo

---
+++ codingpractice/objecttest1/src/Object.java
@@ -0,0 +1,5 @@
+public class Object {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
--- school/java/assignments/discussion1/discussion1/src/discussion1.java
+++
@@ -1,7 +0,0 @@
-public class discussion1 {
-    public static void main(String[] args) { //inputs = (String n, String speech, int numlegs, int numeyes, double weight)
-        Doggo doggo1 = new Doggo("Mr. Cuddlesworth", "Woof", 10, 4, 22.5);
-
-        doggo1.doggostatus();
-    }
-}
