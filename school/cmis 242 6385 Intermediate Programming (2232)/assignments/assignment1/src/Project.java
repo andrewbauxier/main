3a03e5eff8d5660a06a280be3677d3f4ef08cc03
andrewbauxier <andrew.b.auxier@gmail.com>
2023-01-16 21:56:09 +0900

    Create infoblurbs for Project.java method skeleton

--- school/cmis 242 6385 Intermediate Programming (2232)/assignments/assignment1/src/Project.java
+++ school/cmis 242 6385 Intermediate Programming (2232)/assignments/assignment1/src/Project.java
@@ -9,4 +9,39 @@ public class Project {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    //begin methods
+ 
+    // A private class method named findMinimum with a return type of Weight. This method
+    // should accept three Weight objects as parameters and compare each Weight object’s
+    // weight values to find the minimum. The Weight object with the minimum weight value
+    // should be returned and then printed using toString in the following format:
+    // The minimum weight is x pounds and y ounces
+    // where x is the number of pounds and y the number of ounces. Ounces should be
+    // displayed with two decimal places.
+
+    // A private class method named findMaximum with a return type of Weight. This method
+    // should accept three Weight objects as parameters and compare each Weight object’s weight
+    // values to find the maximum. The Weight object with the maximum weight value should be
+    // returned and then printed using toString in the following format:
+    // The maximum weight is x pounds and y ounceswhere x is the number of pounds and y the number of ounces. Ounces should be
+    // displayed with two decimal places.
+
+    // A private class method named findAverage with a return type of Weight. This method
+    // should accept three Weight objects as parameters and calculate the average weight value. A new
+    // Weight object with the average weight values should be returned and then printed using toString
+    // in the following format:
+    // The average weight is x pounds and y ounces
+    // where x is the number of pounds and y the number of ounces. Ounces should be
+    // displayed with two decimal places.
+    
+    // A public method named main with a return type of void. This method should exercise the
+    // correct functionality of findMinimum, findMaximum and findAverage by creating three
+    // Weight objects using the hardcoded values below:
+    // Weight weight1 = new Weight(11, 3);
+    // Weight weight2 = new Weight(7, 20); // Hint: normalize method should be
+    // used to translate into 8 pounds and 4 ounces
+    // Weight weight3 = new Weight(14, 6);
+    
+    //end methods
 }
