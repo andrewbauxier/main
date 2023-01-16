c2a7d2ceb16ec4325370729241873372f761de3d
andrewbauxier <andrew.b.auxier@gmail.com>
2023-01-16 21:39:48 +0900

    Create method skeletons for Weight.java

--- school/cmis 242 6385 Intermediate Programming (2232)/assignments/assignment1/src/Project.java
+++ school/cmis 242 6385 Intermediate Programming (2232)/assignments/assignment1/src/Project.java
@@ -1,6 +1,8 @@
-/* TODO: alter this date before submitting
- * Name: Andrew B. Auxier
- * Date: 2023-01-10 
+/* TODO: alter this before submitting
+ * Name:    Andrew B. Auxier
+ * Class:   Class:CMIS 242 6385
+ * Date:    2023-01-10 
+ * This program does stuff
  * 
 */
 public class Project {
