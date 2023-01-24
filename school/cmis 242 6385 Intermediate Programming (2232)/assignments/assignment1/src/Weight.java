c7aeb91eadf3f150f82d1a90eb076a5c8816e2dc
andrewbauxier <andrew.b.auxier@gmail.com>
2023-01-24 12:13:01 +0900

    Clean up weight class, is finished

--- school/cmis 242 6385 Intermediate Programming (2232)/assignments/assignment1/src/Weight.java
+++ school/cmis 242 6385 Intermediate Programming (2232)/assignments/assignment1/src/Weight.java
@@ -8,39 +8,32 @@
  * 2. A private variable called pounds with a data type of integer.
  * 3. A private variable called ounces with a data type of double
 */
-
 public class Weight {//begin class
     //class attributes
     private int OUNCES_IN_A_POUND = 16;
     private int pounds;
     private double ounces;
-    
     public Weight (int pounds, double ounces) { //begin constructor
         this.pounds=pounds;
         this.ounces=ounces;
     }//end constructor
-
     //begin methods
-    
     /* 
      * A private instance method called toOunces with a return type of double. This method
      * has no parameters and should return the total number of ounces. For full credit, reuse this method
      * across other methods when possible.
     */
-     
     public double toOunces() { //TODO:fix this later
         double totalOunces= (pounds * OUNCES_IN_A_POUND) + ounces; //converts pounds to ounces
         //add existing ounces
         //System.out.println("total ounces: " + totalOunces);
         return totalOunces;
     }
-    
     /*
      * A private instance method called normalize with a return type of void. This method has
      * no parameters and should ensure that the number of ounces is less than the number of ounces in a
      * pound. For full credit, reuse this method across other methods when possible.
     */
-
     public void normalize() {    
         for (int i = 0; this.ounces>OUNCES_IN_A_POUND/*16*/; i++) {//OUNCES_IN_A_POUND=16
                 this.ounces = this.ounces-16;
@@ -49,14 +42,11 @@ public class Weight {//begin class
                 System.out.println("new ounces are : " + this.ounces);
         }
     }
-    //
-
     /* 
      * A public instance method called lessThan with a return type of boolean. This method
      * should accept a Weight object as a parameter and determine if the object is greater or less than
      * the initialized values.
     */
-
      //WORK IN PROGRESS
     public boolean lessThan(Weight weight) { //value to be compared
         return this.toOunces()<weight.toOunces(); //returns true or false depending on values
@@ -66,22 +56,18 @@ public class Weight {//begin class
      * accept a Weight object as a parameter and add the object’s weight values to the initialized
      * values.
     */
-
     public void addTo (Weight weightToBeAdded){
         this.ounces += weightToBeAdded.toOunces();
     }
-
     /* 
      * public instance method called toString with a return type of String. This method has
      * no parameters and should have the following format: x pounds and y ounces
      * where x is the number of pounds and y the number of ounces. Ounces should be
      * displayed with two decimal places. 
     */
-
     public String toString() {    
     String stringToDisplayPoundsAndOunces = this.pounds + " pounds and " + this.ounces + " ounces";
     return stringToDisplayPoundsAndOunces;
     }
-
     //end methods
 }//end class
