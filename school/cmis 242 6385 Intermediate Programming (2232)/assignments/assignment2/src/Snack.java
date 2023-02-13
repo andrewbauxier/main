d5f38be07798a3b2eb6cb87addef2a1e4a98096f
andrewbauxier <andrew.b.auxier@gmail.com>
2023-02-13 10:33:19 +0900

    Create super and child classes

--- school/cmis 242 6385 Intermediate Programming (2232)/assignments/assignment2/src/Snack.java
+++ school/cmis 242 6385 Intermediate Programming (2232)/assignments/assignment2/src/Snack.java
@@ -1,5 +1,25 @@
-public class Snack {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Snack {//begin snack class
+    protected String snackID;             //begin class attributes
+    protected String sizeOfSnack;
+    protected double priceOfSnack;        //end class attributes
+    /* 
+     *  (1) There will be a Snack class with following attributes: id (combination of numbers and letters), 
+        size (values S, M, or L), and price
+    */
+
+    public Snack (String snackID, String sizeOfSnack, Double priceOfSnack) { //begin snack constructor
+        this.snackID=snackID;
+        this.sizeOfSnack=sizeOfSnack;
+        this.priceOfSnack=priceOfSnack;    
+    }//end snack constructor
+
+    public class FruitSnack extends Snack { 
+        private boolean hasFruit = true;
+        public FruitSnack (String snackID, String sizeOfSnack, Double priceOfSnack) {
+            super(snackID, sizeOfSnack, priceOfSnack)
+        }
     }
-}
+// //begin methods
+// private void exampleMethod() { //converts from pounds and ounces to ounces
+// }//end methods
+}//end snack class    
