16ce2da9eec37ff623c6a6f7357cbdfad28b21dc
andrewbauxier <andrew.b.auxier@gmail.com>
2023-03-05 10:35:38 +0900

    Continue after removing code

--- school/cmis 242 6385 Intermediate Programming (2232)/assignments/finalprojectauxier/projectAuxier/src/Ebook.java
+++ school/cmis 242 6385 Intermediate Programming (2232)/assignments/finalprojectauxier/projectAuxier/src/Ebook.java
@@ -2,9 +2,6 @@ public class Ebook extends Media {
     private int numberOfChapters = 0;
     public Ebook(int mediaID, String mediaTitle, int mediaYearPublished, boolean isAvailable) {
         super(mediaID, mediaTitle, mediaYearPublished, isAvailable);
-        this.mediaID = mediaID;
-        this.mediaTitle = mediaTitle;
-        this.mediaYearPublished = mediaYearPublished;
         this.numberOfChapters = numberOfChapters;
     }
 }
--- school/cmis 242 6385 Intermediate Programming (2232)/assignments/finalprojectauxier/projectAuxier/src/MovieDVD.java
+++ school/cmis 242 6385 Intermediate Programming (2232)/assignments/finalprojectauxier/projectAuxier/src/MovieDVD.java
@@ -1,11 +1,7 @@
 public class MovieDVD extends Media {
-    
     private int sizeInMegabytes = 0;
     public MovieDVD(int mediaID, String mediaTitle, int mediaYearPublished, boolean isAvailable) {
         super(mediaID, mediaTitle, mediaYearPublished, isAvailable);
-        this.mediaID = mediaID;
-        this.mediaTitle = mediaTitle;
-        this.mediaYearPublished = mediaYearPublished;
         this.sizeInMegabytes=sizeInMegabytes;
     }
 }
