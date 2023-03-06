2c43d50c96e58e14b3ce2a7f44a626b638438492
andrewbauxier <andrew.b.auxier@gmail.com>
2023-03-06 10:35:20 +0900

    Modify MusicCD class encapsulation

--- school/cmis 242 6385 Intermediate Programming (2232)/assignments/finalprojectauxier/projectAuxier/src/MovieDVD.java
+++ school/cmis 242 6385 Intermediate Programming (2232)/assignments/finalprojectauxier/projectAuxier/src/MovieDVD.java
@@ -1,7 +1,24 @@
 public class MovieDVD extends Media {
-    private int sizeInMegabytes = 0;
+    private double sizeInMegabytes = 0;
     public MovieDVD(int mediaID, String mediaTitle, int mediaYearPublished, boolean isAvailable) {
         super(mediaID, mediaTitle, mediaYearPublished, isAvailable);
         this.sizeInMegabytes=sizeInMegabytes;
     }
+    // get method
+		public double getSize() {
+			return sizeInMegabytes;
+		}
+
+		// set method
+		public void setSize(double sizeInMegabytes) {
+			this.sizeInMegabytes = sizeInMegabytes;
+		}
+
+		// inherits calculate rental fee method and no different calculation so should
+		// not override
+		@Override
+		public String toString() { //display stuff
+			return "MovieDVD [ id=" + getId() + ", title=" + getTitle() + ", year=" + getYear() + ", size=" + getSize()
+					+ "MB, available=" + this.isAvailable() + " ]";
+		}
 }
