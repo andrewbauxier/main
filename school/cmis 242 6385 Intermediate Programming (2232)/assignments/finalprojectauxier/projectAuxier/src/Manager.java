2e2155dc11c2294640a576ad4641078b3f1e882c
andrewbauxier <andrew.b.auxier@gmail.com>
2023-03-06 09:45:45 +0900

    Modify media class encapsulation

--- school/cmis 242 6385 Intermediate Programming (2232)/assignments/finalprojectauxier/projectAuxier/src/Manager.java
+++ school/cmis 242 6385 Intermediate Programming (2232)/assignments/finalprojectauxier/projectAuxier/src/Manager.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Manager {
     public static void main(String[] args) {
-        
+        runMenu();
+    }
+
+    public static void runMenu() {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Welcome to Media Rental System.\n1: Load Media objects..."
+            + "2: Find Media object...\n3: Rent Media object...\n9: Quit\n\nEnter your selection:\t");
+        int menuInput = scanner1.nextInt();
+        menuSwitching(menuInput);
+    }
+    public static void menuSwitching(int menuInput) {
+        switch (menuInput) {
+            case 1: //Load meadia objects method
+                
+                break;
+            case 2: //Find media object method
+                
+                break;
+            case 3: //Rent Media object
+                
+                break; 
+            case 9: //end program
+            
+            break;   
+            default:
+                break;
+        }
     }
 }
--- school/cmis 242 6385 Intermediate Programming (2232)/assignments/finalprojectauxier/projectAuxier/src/Media.java
+++ school/cmis 242 6385 Intermediate Programming (2232)/assignments/finalprojectauxier/projectAuxier/src/Media.java
@@ -1,4 +1,4 @@
-public class Media {
+public abstract class Media {
     /* Member variables (attributes): id (integer), title (string), year published (integer)
     Functionality: constructor with all the attribute values, calculate rental fee ($3.50 flat
 fee), change title, change year published, and get each attribute value.
@@ -7,11 +7,12 @@ want to allow a creation of this class directly
  Note that id value cannot be changed after creation.
      * 
     */
-    int mediaID;
-    String mediaTitle;
-    int mediaYearPublished;
-    double rentalFee = 3.50;
-    boolean isAvailable;
+    private int mediaID;
+    private String mediaTitle;
+    private int mediaYearPublished;
+    //private double rentalFee = 3.50;
+        //maybe dont put this here
+    private boolean isAvailable;
     public Media(int mediaID, String mediaTitle, int mediaYearPublished, boolean isAvailable) { //needs get,
         this.mediaID = mediaID;
         this.mediaTitle = mediaTitle;
