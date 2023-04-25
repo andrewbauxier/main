6a396b65d0528a731bacb1816fd744f4954f9796
andrewbauxier <andrew.b.auxier@gmail.com>
2023-04-25 22:09:39 +0900

    Modify two SQL files, crap ton of inserts and stuff

--- school/SQL/CREATETABLES AUXIER.sql
+++ school/SQL/CREATETABLES AUXIER.sql
@@ -4,35 +4,35 @@
 --DATE: 20230422
 
 CREATE TABLE ACTOR (
-  ActorID           NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  ActorID           INT PRIMARY KEY NOT NULL,
   FirstName         VARCHAR2(30) , 
   LastName          VARCHAR2(30) 
 );
 
-CREATE TABLE DIRECTOR (
-  DirectorID        NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-  FirstName         VARCHAR2(30) , 
-  LastName          VARCHAR2(30) 
-);
-
-CREATE TABLE AWARD (
-  AwardID           NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,   
-  DateReceived      DATE , 
-  TypeOfAward       VARCHAR2(10)  
-);
-
----MOVIE TABLE
 CREATE TABLE MOVIE (
-  MovieID           NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,    
-  DistributorID     VARCHAR2(30) , 
-  RunningLength     NUMBER(6) , 
+  MovieID           INT PRIMARY KEY NOT NULL,
+  DistributorID     NUMBER(30) ,
+  MovieName         VARCHAR2(30) ,
+  RunningLength     NUMBER(10) , 
   Genre             VARCHAR2(30) , 
   Rating            VARCHAR2(30) , 
   ReleaseDate       DATE 
 );
 
+CREATE TABLE DIRECTOR (
+  DirectorID        INT PRIMARY KEY NOT NULL,
+  FirstName         VARCHAR2(30) , 
+  LastName          VARCHAR2(30) 
+);
+
+CREATE TABLE AWARD (
+  AwardID           INT PRIMARY KEY NOT NULL,
+  AwardName         VARCHAR2(10)  
+  DateReceived      DATE , 
+);
+
 CREATE TABLE CUSTOMER (
-  CustomerID        NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,   
+  CustomerID        INT PRIMARY KEY NOT NULL, 
   FirstName         VARCHAR2(30) , 
   LastName          VARCHAR2(30) ,
   DateOfBirth       DATE , 
@@ -41,30 +41,29 @@ CREATE TABLE CUSTOMER (
   StateID           VARCHAR2(2) ,
   ZipCode           NUMBER(30) ,
   Telephone         NUMBER(10) 
-
 );
 
 CREATE TABLE DVD (
-  DVDID             NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,    
+  DVDID             INT PRIMARY KEY NOT NULL, 
   Title             VARCHAR2(30) , 
   RunningLength     NUMBER(6) , 
   Genre             VARCHAR2(30) , 
   RentalFee         NUMBER(10) ,
-  Discount          NUMBER(2) 
+  Discount          NUMBER(3), 
 );
 
 CREATE TABLE VHS (
-  VHSID             NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,    
+  VHSID             INT PRIMARY KEY NOT NULL,
   Title             VARCHAR2(30) , 
   RunningLength     NUMBER(6) , 
   Genre             VARCHAR2(30) , 
   RentalFee         NUMBER(10) ,
-  Discount          NUMBER(2) 
+  Discount          NUMBER(3) 
 );
 
 CREATE TABLE RENTAL (
-  RentalID          NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,    
-  DistributorID     VARCHAR2(30) , 
+  RentalID          INT PRIMARY KEY NOT NULL,    
+  DistributorID     INT(30) , 
   DateOut           DATE , 
   DateIn            DATE , 
   DateDue           DATE ,
@@ -77,22 +76,26 @@ CREATE TABLE RENTAL (
 
 ----------------------
 
---RELATIONAL TABLES
+--RELATIONAL TABLES---------------------------------
 CREATE TABLE ACTOR_MOVIE (
-  ActorID           NUMBER(8) , 
-  MovieID           NUMBER(8) ,
-  CONSTRAINT ActorID_ACTOR_MOVIE_FK1    FOREIGN KEY (ActorID) REFERENCES ACTOR(ActorID) ,
-  CONSTRAINT MovieID_ACTOR_MOVIE_FK2    FOREIGN KEY (MovieID) REFERENCES MOVIE(MovieID) 
-);
-CREATE TABLE ACTOR_MOVIE (
+  ACTORMOVIEID      INT PRIMARY KEY NOT NULL,
   ActorID           NUMBER, 
   MovieID           NUMBER,
-  CONSTRAINT Actor_Movie_PK PRIMARY KEY (ActorID, MovieID),
   CONSTRAINT Actor_Movie_Actor_FK FOREIGN KEY (ActorID) REFERENCES Actor(ActorID),
   CONSTRAINT Actor_Movie_Movie_FK FOREIGN KEY (MovieID) REFERENCES Movie(MovieID)
 );
 
+-- CREATE TABLE ACTOR_MOVIE (
+--   ACTORMOVIEID      INT PRIMARY KEY NOT NULL,
+--   ActorID           NUMBER REFERENCES ACTOR(ActorID),
+--   MovieID           NUMBER REFERENCES MOVIE(MovieID),
+--   Role              VARCHAR2(30)
+-- );
+
+---------------------------------------------------
+
 CREATE TABLE DIRECTOR_MOVIE (
+  DIRECTORMOVIEID   INT PRIMARY KEY NOT NULL,
   DirectorID        NUMBER(8) , 
   MovieID           NUMBER(8) ,
   CONSTRAINT DirectorID_DIRECTOR_MOVIE_FK1 FOREIGN KEY (DirectorID) REFERENCES DIRECTOR(DirectorID)  ,
@@ -100,6 +103,7 @@ CREATE TABLE DIRECTOR_MOVIE (
 );
 
 CREATE TABLE AWARD_MOVIE (
+  AWARDMOVIEID      INT PRIMARY KEY NOT NULL,
   AwardID           NUMBER(8) , 
   MovieID           NUMBER(8) ,
   CONSTRAINT AwardID_AWARD_MOVIE_FK1    FOREIGN KEY (AwardID) REFERENCES AWARD(AwardID) ,
@@ -107,30 +111,39 @@ CREATE TABLE AWARD_MOVIE (
 );
 
 CREATE TABLE DVD_MOVIE (
+  DVDMOVIEID        INT PRIMARY KEY NOT NULL,
   DVDID             NUMBER(8) , 
   MovieID           NUMBER(8) ,
   CONSTRAINT DVDID_DVD_MOVIE_FK1      FOREIGN KEY (DVDID) REFERENCES DVD(DVDID) ,
   CONSTRAINT MovieID_DVD_MOVIE_FK2    FOREIGN KEY (MovieID) REFERENCES MOVIE(MovieID) 
 );
+
 CREATE TABLE VHS_MOVIE (
+  VHSMOVIEID        INT PRIMARY KEY NOT NULL,
   VHSID             NUMBER(8) , 
   MovieID           NUMBER(8) ,
   CONSTRAINT VHSID_VHS_MOVIE_FK1      FOREIGN KEY (VHSID) REFERENCES VHS(VHSID) ,
   CONSTRAINT MovieID_VHS_MOVIE_FK2    FOREIGN KEY (MovieID) REFERENCES MOVIE(MovieID) 
 );
+
 CREATE TABLE VHS_RENTAL (
+  VHSRENTALID       INT PRIMARY KEY NOT NULL,
   VHSID             NUMBER(8) , 
   RentalID          NUMBER(8) ,
   CONSTRAINT VHSID_VHS_RENTAL_FK1      FOREIGN KEY (VHSID) REFERENCES VHS(VHSID) ,
   CONSTRAINT RentalID_VHS_RENTAL_FK2   FOREIGN KEY (RentalID) REFERENCES RENTAL(RentalID) 
 );
+
 CREATE TABLE DVD_RENTAL (
+  DVDRENTALID       INT PRIMARY KEY NOT NULL,
   DVDID             NUMBER(8) , 
   RentalID          NUMBER(8) ,
   CONSTRAINT DVDID_DVD_RENTAL_FK1      FOREIGN KEY (DVDID) REFERENCES DVD(DVDID) ,
   CONSTRAINT RentalID_DVD_RENTAL_FK2   FOREIGN KEY (RentalID) REFERENCES RENTAL(RentalID) 
 );
+
 CREATE TABLE CUSTOMER_RENTAL (
+  CUSTOMER_RENTALID INT PRIMARY KEY NOT NULL,
   CustomerID        NUMBER(8) , 
   RentalID          NUMBER(8) ,
   CONSTRAINT CustomerID_CUSTOMER_RENTAL_FK1 FOREIGN KEY (CustomerID) REFERENCES CUSTOMER(CustomerID) ,
--- school/SQL/INSERTINTO AUXIER.sql
+++ school/SQL/INSERTINTO AUXIER.sql
@@ -5,54 +5,140 @@
 
 --MAIN TABLES
 --ACTOR
+INSERT INTO ACTOR (ActorID, FirstName, LastName) VALUES (1001, 'Tom', 'Hanks');
 
-INSERT INTO ACTOR 
-	--(ActorID, FirstName, LastName)
-	VALUES (1, 'ACTORFIRST1', 'ACTORLAST1') ;
-INSERT INTO ACTOR 
-	VALUES (2, 'ACTORFIRST2', 'ACTORLAST2') ;
-INSERT INTO ACTOR 
-	VALUES (3, 'ACTORFIRST3', 'ACTORLAST3') ;
-INSERT INTO ACTOR 
-	VALUES (4, 'ACTORFIRST4', 'ACTORLAST4') ;
-INSERT INTO ACTOR
-	
-	VALUES (5, 'ACTORFIRST5', 'ACTORLAST5') ;
+INSERT INTO ACTOR (ActorID, FirstName, LastName) VALUES (1002, 'Meryl', 'Streep');
+
+INSERT INTO ACTOR (ActorID, FirstName, LastName) VALUES (1003, 'Brad', 'Pitt');
+
+INSERT INTO ACTOR (ActorID, FirstName, LastName) VALUES (1004, 'Emma', 'Stone');
+
+INSERT INTO ACTOR (ActorID, FirstName, LastName) VALUES (1005, 'Denzel', 'Washington');
 
 --DIRECTOR
-INSERT INTO DIRECTOR
-	(DirectorID, FirstName, LastName)
-	VALUES (1, 'DIRECTORFIRST1', 'DIRECTORLAST1') ;
-INSERT INTO DIRECTOR
-	VALUES (2, 'DIRECTORFIRST2', 'DIRECTORLAST2') ;
-INSERT INTO DIRECTOR
-	VALUES (3, 'DIRECTORFIRST3', 'DIRECTORLAST3') ;
-INSERT INTO DIRECTOR
-	VALUES (4, 'DIRECTORFIRST4', 'DIRECTORLAST4') ;
-INSERT INTO DIRECTOR
-	VALUES (5, 'DIRECTORFIRST5', 'DIRECTORLAST5') ;
+INSERT INTO DIRECTOR (DirectorID, FirstName, LastName) VALUES (001, 'Christopher', 'Nolan');
+
+INSERT INTO DIRECTOR (DirectorID, FirstName, LastName) VALUES (1002, 'Steven', 'Spielberg');
+
+INSERT INTO DIRECTOR (DirectorID, FirstName, LastName) VALUES (1002, 'Steven', 'Spielberg');
+
+INSERT INTO DIRECTOR (DirectorID, FirstName, LastName) VALUES (1004, 'Ava', 'DuVernay');
+
+INSERT INTO DIRECTOR (DirectorID, FirstName, LastName) VALUES (1005, 'Jordan', 'Peele');
+
 --AWARD
-INSERT INTO AWARD
-	(AwardID, DateReceived, LastName)
-	VALUES (1, 'DIRECTORFIRST1', 'DIRECTORLAST1') ;
+INSERT INTO AWARD (AwardID, AwardName, DateReceived, ActorFirstName, ActorLastName)
+VALUES (1001, 'Best Supporting Actor', TO_DATE('2022-03-15', 'YYYY-MM-DD'), 'Tom', 'Hanks');
+
+INSERT INTO AWARD (AwardID, AwardName, DateReceived, ActorFirstName, ActorLastName)
+VALUES (1002, 'Best Director', TO_DATE('2022-03-15', 'YYYY-MM-DD'), 'Chloe', 'Zhao');
+
+INSERT INTO AWARD (AwardID, AwardName, DateReceived, ActorFirstName, ActorLastName)
+VALUES (1003, 'Best Picture', TO_DATE('2022-03-15', 'YYYY-MM-DD'), 'Steven', 'Spielberg');
+
+INSERT INTO AWARD (AwardID, AwardName, DateReceived, ActorFirstName, ActorLastName)
+VALUES (1004, 'Best Original Screenplay', TO_DATE('2022-03-15', 'YYYY-MM-DD'), 'Aaron', 'Sorkin');
+
+INSERT INTO AWARD (AwardID, AwardName, DateReceived, ActorFirstName, ActorLastName)
+VALUES (1005, 'Best Cinematography', TO_DATE('2022-03-15', 'YYYY-MM-DD'), 'Roger', 'Deakins');
 
 
 --MOVIE
+INSERT INTO MOVIE (DistributorID, MovieName, RunningLength, Genre, Rating, ReleaseDate)
+VALUES (1001, 'The Avengers', 120, 'Action', 'PG-13', TO_DATE('2022-05-15', 'YYYY-MM-DD'));
+
+INSERT INTO MOVIE (DistributorID, MovieName, RunningLength, Genre, Rating, ReleaseDate)
+VALUES (1002, 'The Dark Knight', 150, 'Drama', 'R', TO_DATE('2022-07-20', 'YYYY-MM-DD'));
+
+INSERT INTO MOVIE (DistributorID, MovieName, RunningLength, Genre, Rating, ReleaseDate)
+VALUES (1003, 'Bridesmaids', 90, 'Comedy', 'PG', TO_DATE('2022-06-01', 'YYYY-MM-DD'));
+
+INSERT INTO MOVIE (DistributorID, MovieName, RunningLength, Genre, Rating, ReleaseDate)
+VALUES (1004, 'Wonder Woman', 130, 'Action', 'PG-13', TO_DATE('2022-06-15', 'YYYY-MM-DD'));
+
+INSERT INTO MOVIE (DistributorID, MovieName, RunningLength, Genre, Rating, ReleaseDate)
+VALUES (1005, 'Get Out', 120, 'Horror', 'R', TO_DATE('2022-08-05', 'YYYY-MM-DD'));
 
 
 --CUSTOMER
+INSERT INTO CUSTOMER (CustomerID, FirstName, LastName, DateOfBirth, StreetAddress, City, StateID, ZipCode, Telephone)
+VALUES (1001, 'John', 'Doe', TO_DATE('1985-06-12', 'YYYY-MM-DD'), '123 Main St', 'Anytown', 'NY', 12345, 5551234567);
+
+INSERT INTO CUSTOMER (CustomerID, FirstName, LastName, DateOfBirth, StreetAddress, City, StateID, ZipCode, Telephone)
+VALUES (1002, 'Jane', 'Smith', TO_DATE('1990-02-15', 'YYYY-MM-DD'), '456 Oak St', 'Otherville', 'CA', 98765, 5559876543);
+
+INSERT INTO CUSTOMER (CustomerID, FirstName, LastName, DateOfBirth, StreetAddress, City, StateID, ZipCode, Telephone)
+VALUES (1003, 'Bob', 'Johnson', TO_DATE('1980-11-20', 'YYYY-MM-DD'), '789 Maple Ave', 'Maplewood', 'NJ', 07040, 5555551212);
+
+INSERT INTO CUSTOMER (CustomerID, FirstName, LastName, DateOfBirth, StreetAddress, City, StateID, ZipCode, Telephone)
+VALUES (1004, 'Sara', 'Lee', TO_DATE('1995-07-04', 'YYYY-MM-DD'), '101 Cherry Ln', 'Cherryville', 'GA', 30303, 5557778888);
+
+INSERT INTO CUSTOMER (CustomerID, FirstName, LastName, DateOfBirth, StreetAddress, City, StateID, ZipCode, Telephone)
+VALUES (1005, 'Mike', 'Johnson', TO_DATE('1988-09-05', 'YYYY-MM-DD'), '246 Elm St', 'Elmtown', 'IL', 60601, 5559998888);
 
 
 --DVD
+-- Example 1
+INSERT INTO DVD (DVDID, Title, RunningLength, Genre, RentalFee, Discount)
+VALUES (1001, 'The Shawshank Redemption', 142, 'Drama', 3.99, 10);
+
+-- Example 2
+INSERT INTO DVD (DVDID, Title, RunningLength, Genre, RentalFee, Discount)
+VALUES (1002, 'The Dark Knight', 152, 'Action', 4.99, 5);
+
+-- Example 3
+INSERT INTO DVD (DVDID, Title, RunningLength, Genre, RentalFee, Discount)
+VALUES (1003, 'Forrest Gump', 142, 'Drama', 3.99, 15);
+
+-- Example 4
+INSERT INTO DVD (DVDID, Title, RunningLength, Genre, RentalFee, Discount)
+VALUES (1004, 'The Godfather', 175, 'Drama', 3.99, 10);
+
+-- Example 5
+INSERT INTO DVD (DVDID, Title, RunningLength, Genre, RentalFee, Discount)
+VALUES (1005, 'The Lord of the Rings: The Fellowship of the Ring', 178, 'Fantasy', 4.99, 5);
 
 
 --VHS
+-- Example 1
+INSERT INTO VHS (VHSID, Title, RunningLength, Genre, RentalFee, Discount)
+VALUES (1001, 'The Lion King', 89, 'Animation', 2.99, 10);
+
+-- Example 2
+INSERT INTO VHS (VHSID, Title, RunningLength, Genre, RentalFee, Discount)
+VALUES (1002, 'Ghostbusters', 105, 'Comedy', 2.99, 5);
+
+-- Example 3
+INSERT INTO VHS (VHSID, Title, RunningLength, Genre, RentalFee, Discount)
+VALUES (1003, 'Terminator 2: Judgment Day', 137, 'Action', 2.99, 15);
+
+-- Example 4
+INSERT INTO VHS (VHSID, Title, RunningLength, Genre, RentalFee, Discount)
+VALUES (1004, 'Pretty Woman', 119, 'Romance', 2.99, 10);
+
+-- Example 5
+INSERT INTO VHS (VHSID, Title, RunningLength, Genre, RentalFee, Discount)
+VALUES (1005, 'The Silence of the Lambs', 118, 'Thriller', 2.99, 5);
 
 
 --RENTAL
+INSERT INTO RENTAL (RentalID, DistributorID, DateOut, DateIn, DateDue, RentalFee, LateFee, DamageFee, RewindFee, OtherFee)
+VALUES (1001, 1001, TO_DATE('2022-02-01', 'YYYY-MM-DD'), TO_DATE('2022-02-02', 'YYYY-MM-DD'), TO_DATE('2022-02-08', 'YYYY-MM-DD'), 5.99, 1.50, 0.00, 0.00, 0.00);
 
+INSERT INTO RENTAL (RentalID, DistributorID, DateOut, DateIn, DateDue, RentalFee, LateFee, DamageFee, RewindFee, OtherFee)
+VALUES (1002, 1002, TO_DATE('2022-03-15', 'YYYY-MM-DD'), TO_DATE('2022-03-16', 'YYYY-MM-DD'), TO_DATE('2022-03-22', 'YYYY-MM-DD'), 3.99, 0.00, 0.00, 0.50, 0.00);
+
+INSERT INTO RENTAL (RentalID, DistributorID, DateOut, DateIn, DateDue, RentalFee, LateFee, DamageFee, RewindFee, OtherFee)
+VALUES (1003, 1003, TO_DATE('2022-05-10', 'YYYY-MM-DD'), TO_DATE('2022-05-13', 'YYYY-MM-DD'), TO_DATE('2022-05-17', 'YYYY-MM-DD'), 2.99, 1.50, 0.00, 0.00, 0.00);
+
+INSERT INTO RENTAL (RentalID, DistributorID, DateOut, DateIn, DateDue, RentalFee, LateFee, DamageFee, RewindFee, OtherFee)
+VALUES (1001, 1004, TO_DATE('2023-04-23', 'YYYY-MM-DD'), TO_DATE('2023-04-25', 'YYYY-MM-DD'), TO_DATE('2023-04-28', 'YYYY-MM-DD'), 5.99, 0, 0, 0.50, 0);
+
+INSERT INTO RENTAL (RentalID, DistributorID, DateOut, DateIn, DateDue, RentalFee, LateFee, DamageFee, RewindFee, OtherFee)
+VALUES (1002, 1005, TO_DATE('2023-04-20', 'YYYY-MM-DD'), TO_DATE('2023-04-25', 'YYYY-MM-DD'), TO_DATE('2023-04-27', 'YYYY-MM-DD'), 3.99, 1.50, 0, 0.50, 0);
 
 --RELATIONAL TABLES
+
 --ACTOR_MOVIE
 
 
