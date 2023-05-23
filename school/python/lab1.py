70016856daab6d394428b683ce91077789f58a30
anchan <andrew.b.auxier@gmail.com>
2023-05-23 10:19:59 +0900

    Finish a few function and work on zipcode and state

--- school/python/lab1.py
+++ school/python/lab1.py
@@ -29,45 +29,71 @@
 
 ##################### functions begin
 # loop to introduce program and provide ability to exit program. also performs validation of options
+def exitAtAnyJunction():
+  exitVariable = input("\nDo you wish to continue with registration? Type 'yes' or 'no'.\n")
+  exitVariable = exitVariable.lower()
+  if exitVariable == "no":
+    exit(0)
+
 def enterExitLoop():
   while True:
     enterExitLoopInput = input("\nWelcome to the Python Voter Registration Application. Do you want to continue with Voter Registration? Type 'yes' or 'no'\n")
     enterExitLoopInput = enterExitLoopInput.lower()
     if enterExitLoopInput == "yes":
-        userBool = True
+        #userBool = True
         enterUserFirstAndLastName()
-        #break
+        
     elif enterExitLoopInput == "no":
-        userBool = False
+        #userBool = False
         print("\nExiting the program.\n")
         exit(0)
     else:
         print("Invalid input. Please enter 'yes' or 'no'.")
-  print("The boolean value is:", userBool)    
+    #print("The boolean value is:", userBool) #
       
 ## function to acquire first and last name of registrant
 def enterUserFirstAndLastName():
-    userFirstName = input("\nIf you wish to exit the program at any time, type 'exit'.\nPlease enter your first name now\n")
-    if userFirstName == "exit":
-      exit(0)
+    userFirstName = input("\nPlease enter your first name now\n")
     userLastName = input("\nPlease enter your last name now\n")
-    if userLastName == "exit":
-      exit(0)
     print("\nThe name you entered is:\n", userFirstName, userLastName)
+    exitAtAnyJunction()
     enterUserAgeAndValidate()
 
 ## function to gather user age input and validate age
 def enterUserAgeAndValidate():
-  userAge = input("\nBe advised, only individuals 18 years og age or older are allowed to register.\nPlease input your age now")
-  if userAge == "exit":
+  userAge = int(input("\nBe advised, only individuals 18 years of age or older are allowed to register.\nPlease input your age now\n"))
+  if userAge > 17 and userAge < 130:
+    print("\nThe age you entered is: ",  userAge)
+    exitAtAnyJunction()
+    isUSCitizenCheck()
+  else:
+    print("You must be 18 or older to register to vote. Goodbye.")
     exit(0)
+    
+def isUSCitizenCheck():
+  while True:
+    isUserUSCitizen = input("\nAre you a U.S. citizen? Please enter 'yes' or 'no' now.\n")
+    if isUserUSCitizen.isalpha:
+      isUserUSCitizen = isUserUSCitizen.lower()
+      if isUserUSCitizen == "yes":
+        print("ok")
+      #go to next section
+      elif isUserUSCitizen == "no":
+        print("You must be a U.S. Citizen to register to vote. Goodbye.")
+        exit(0) 
+    else:
+      print("\nInvalid input. Please enter 'yes' or 'no'.")
+      
+def enterUserZipCode():
+  userZipCode = int(input("\nPlease enter your zip code now"))
+    
+        
 ##################### functions end
 
 #######################################################################################################################
 
 #main program 
-enterExitLoop()
-
-
-
-
+#enterExitLoop()
+#enterUserFirstAndLastName()
+#enterUserAgeAndValidate()
+#isUSCitizenCheck()
