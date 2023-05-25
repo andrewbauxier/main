777fb9c69afd3d22d873536dbe1ba4c2de9e4eb3
andrewbauxier <andrew.b.auxier@gmail.com>
2023-05-26 08:54:48 +0900

    Made minor changes

---
+++ school/python/lab2.py
@@ -0,0 +1,8 @@
+"""
+Title:          Lab 1
+Name:           Andrew Auxier
+Course:         SDEV 300 6383 Building Secure Python Applications (2235)
+Description:    This project serves a multitude of functions as listed: Generate a secure password,\
+    
+"""
+
