5398b5e882d9e6735eb0a8b8fba4aeabf6d2d975
andrewbauxier <andrew.b.auxier@gmail.com>
2023-05-26 12:33:47 +0900

    Create main menu loop

--- school/python/lab2.py
+++ school/python/lab2.py
@@ -3,6 +3,30 @@ Title:          Lab 1
 Name:           Andrew Auxier
 Course:         SDEV 300 6383 Building Secure Python Applications (2235)
 Description:    This project serves a multitude of functions as listed: Generate a secure password,\
+    Calculate and Format a Percentage, determine number of Days until July 4, 2025, use the Law of \
+    Cosines to calculate the leg of a triangle, calculate the volume of a Right Circular Cylinder, \
+    and exit the program.
     
 """
 
+## menu loop
+def main_menu_loop():
+    while True:
+        print("Welcome to the Assorted Utility Program. You options are as follows: \n")
+        print("1: Secure Password Generation\n2:Calculate and Format a Percentage\n \
+            3: Determine the number of Days until July 4, 2025\n4: Calculate the leg of a triangle \
+            \n5: Calculate the volume of a Right Circular Cylinder\n6: Exit the program\n")
+        int(input("Which option would you like to choose?"))
+## Generate a secure password
+
+## Calculate and Format a Percentage
+
+## Determine number of Days until July 4, 2025
+
+## Use the Law of Cosines to calculate the leg of a triangle
+
+## Calculate the volume of a Right Circular Cylinder
+
+## Exit the program
+
+main_menu_loop()
