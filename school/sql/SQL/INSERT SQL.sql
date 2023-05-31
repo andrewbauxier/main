53cee263508b856c9790800b820f15f22bac1b76
andrewbauxier <andrew.b.auxier@gmail.com>
2023-05-31 10:46:56 +0900

    Ignore last commit, now actually fix the date issue, did not export from sqldev

--- school/sql/SQL/INSERT SQL.sql
+++ school/sql/SQL/INSERT SQL.sql
@@ -3,7 +3,7 @@ SELECT
     'Last'||(ROWNUM) AS Lastname,
     'First'||(ROWNUM) AS Firstname,
     'example'||(ROWNUM)||'@example.com' AS Email,
-    TO_DATE('01-JAN-'||(2023+ROWNUM), 'DD-MON-YYYY') AS Graddate
+    DATE '2023-01-01' + (ROWNUM-1) AS Graddate
 FROM
     dual
 CONNECT BY
