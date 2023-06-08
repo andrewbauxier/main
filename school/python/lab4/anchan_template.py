00a4cb8c0f77b9f35bf99468612d8bc72402c716
andrewbauxier <andrew.b.auxier@gmail.com>
2023-06-08 12:27:08 +0900

    Begin lab 4; Start making skeletons
    ok so this lab, like the others is hefty. the first thing to do is make
    the skeletons for the modules. after that, i'll decide what functions
    are needed. then I'll -do some stuff-™

---
+++ school/python/lab4/anchan_template.py
@@ -0,0 +1,18 @@
+"""
+Project Name:   Lab 4
+Module Name:    main.py
+Author:         Andrew Auxier
+Company:        UMGC, SDEV 300 6383 Building Secure Python Applications (2235) 
+Description: 
+
+    This project serves several functions as listed below:
+"""
+# import something
+
+
+def func():  #
+    print()
+
+
+# Start the program
+func()
--- school/python/lab4/main.py
+++ school/python/lab4/main.py
@@ -20,5 +20,25 @@ def main_menu_loop():  #
         anchan_match_case.main_menu_loop()
 
 
+# part 1
+# enter and validate phone number and zip+4.
+# user enters values of two 3x3 matrices
+# select from options including addition, subtraction, matrix multiplication, and
+#     element by element multiplication
+#     should use numpy.matmul() for matrix multiplication (e.g. np.matmul(a, b) )
+# program should:
+#   compute the appropriate results
+#   return the results and the transpose of the results, the mean of the rows for the results,
+#       and the mean of the columns for the results
+# steps
+#   make skeletons for modules
+#   make skeletons for functions
+#   work on functions seperately
+#
+#
+#
+#
+
+
 # Start the program
 main_menu_loop()
