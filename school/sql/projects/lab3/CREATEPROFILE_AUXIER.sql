7fa9bbea92667a00945829f0ab23676b3533a40e
andrewbauxier <andrew.b.auxier@gmail.com>
2023-06-12 12:56:14 +0900

    Create SQL file for lab 3, being work

---
+++ school/sql/projects/lab3/CREATEPROFILE_AUXIER.sql
@@ -0,0 +1,13 @@
+/*
+1. Create a unique Profile based on the following requirements:
+    a. Password complexity should meet requirements for Ora12 Verify function.
+    b. User may have up to 3 concurrent sessions.
+    c. User may have up to 4 consecutive failed attempts to log in before the account is
+    locked.
+    d. User may wait up till 120 days before their password must be changed.
+    e. User account will be locked for 1 hours after the specified number of consecutive failed
+    login attempts.
+    f. Default values for other Profile parameters is acceptable
+    g. You should name the Profile PFirstnameLastname where Lastname and Firstname are
+    your First and Lastname
+/*
