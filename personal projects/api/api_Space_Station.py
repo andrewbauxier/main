7c6bf5ecf1f07dba35bb5db8f3918c77b771ec08
andrewbauxier <andrew.b.auxier@gmail.com>
2024-07-23 12:14:20 +0900

    CLEAN: Clean up github, restart ODIN

--- personal projects/api/api_Space_Station.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-response = requests.get("https://pokeapi.co/    ")
-print(response.status_code)
---
+++ personal projects/odin/personal_website/src/scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+/**
+ * Load the navigation HTML and insert it into the page.
+ */
+function loadNavigation() {
+    fetch('nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('body').insertAdjacentHTML('afterbegin', data);
+        })
+        .catch(error => console.error('Error loading the navigation:', error));
+}
+
+// Call the function to load the navigation when the page loads
+document.addEventListener('DOMContentLoaded', loadNavigation);
