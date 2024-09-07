c35bede19bb92241204fcec81128933377a59873
andrewbauxier <andrew.b.auxier@gmail.com>
2024-09-07 16:34:37 +0900

    FEAT: Implm server, work on getting things to load

--- projects/aaDesign/app/src/js/server.js
+++ projects/aaDesign/app/src/js/server.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//middleware
-app.use(express.static('src'));
+// Log the current directory
+console.log("LOG: __dirname is", __dirname);
 
+// Correct the static path by moving up two levels from 'js' to the project root
+const staticPath = path.join(__dirname, '..', '..', 'src');
+console.log("LOG - STATIC PATH: Serving static files from", staticPath);
 
+// Serve static files from the correct 'app/src' directory
+app.use(express.static(staticPath));
 
-app.get('/', (req, res)=>{
-    res.send("hello")
-})
+// Correct the path to contact.html by moving up two levels
+const contactHtmlPath = path.join(__dirname, '..', '..', 'src', 'contact.html');
+console.log("LOG - Path to contact.html:", contactHtmlPath);
 
-app.listen(PORT, ()=> {
-    console.log(`Server running on port ${PORT}`) 
-})
+// Serve the contact.html file
+app.get('/', (req, res) => {
+    console.log("LOG: Serving contact.html from", contactHtmlPath);
+    res.sendFile(contactHtmlPath);
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
